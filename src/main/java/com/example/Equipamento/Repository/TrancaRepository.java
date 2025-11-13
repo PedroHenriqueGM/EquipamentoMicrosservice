20673f316c3e6b816d64f3e053a37b7f0776dfd5
Raul Rodrigues <91432196+RaulRJ5@users.noreply.github.com>
2025-11-13 00:32:34 -0300

    atualizando repository

--- src/main/java/com/example/Equipamento/Repository/TrancaRepository.java
+++ src/main/java/com/example/Equipamento/Repository/TrancaRepository.java
@@ -5,6 +5,7 @@ import com.example.Equipamento.Model.Tranca;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface TrancaRepository extends JpaRepository<Tranca, Integer> {
@@ -16,4 +17,6 @@ public interface TrancaRepository extends JpaRepository<Tranca, Integer> {
 
     // === R4: existe alguma tranca ocupando esta bicicleta? ===
     boolean existsByBicicletaId(Integer bicicletaId);
+
+    List<Tranca> findByTotemId(Long totemId);
 }
